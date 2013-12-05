28c918bae8cdc19c987b9f05330414f45087ab89
Nilesh Pambhar <nileshp@amphee.com>
2013-12-05 11:47:36 +0530

    Hello from wardroba

---
+++ wardroba/src/com/example/wardroba/HomeActivity.java
@@ -0,0 +1,27 @@
+package com.example.wardroba;
+
+import com.TabBar.TabHostProvider;
+import com.TabBar.TabView;
+import com.TabBar.TabbarView;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class HomeActivity extends Activity
+{
+	public TabHostProvider tabProvider;
+    public TabView tabView;
+
+    public void onCreate(Bundle savedInstanceState) 
+    {
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+	    super.onCreate(savedInstanceState);       
+	    
+	 	 
+	    tabProvider = new TabbarView(this);
+		tabView = tabProvider.getTabHost();
+		tabView.setCurrentView(R.layout.home_activity);
+		setContentView(tabView.render(0)); 
+    }
+}
